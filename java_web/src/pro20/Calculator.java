5b96ed3bb6cc82f54266cc3b8ed824b0ccc13c7a
Cat Hanbit <dudtlr102@naver.com>
2022-08-15 21:30:56 +0900

    calculator

---
+++ java_web/src/pro20/Calculator.java
@@ -0,0 +1,23 @@
+package pro20;
+
+public class Calculator {
+	public void add(int x, int y) {
+		int result=x+y;
+		System.out.println("°á°ú:"+ result);
+	}
+
+	public void subtract(int x, int y) {
+		int result=x - y;
+		System.out.println("°á°ú:"+ result);
+	}
+
+	public void multiply(int x, int y) {
+		int result=x * y;
+		System.out.println("°á°ú:"+ result);
+	}
+
+	public void divide(int x, int y) {
+		int result=x / y;
+		System.out.println("°á°ú:"+ result);
+	}
+}
