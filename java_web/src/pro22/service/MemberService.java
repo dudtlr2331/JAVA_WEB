1eb551f3c25a6549803982b9a127e2c1a3e6ddb0
Cat Hanbit <dudtlr102@naver.com>
2022-08-27 23:19:40 +0900

    memberService

---
+++ java_web/src/pro22/service/MemberService.java
@@ -0,0 +1,8 @@
+package pro22.service;
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+public interface MemberService {
+	public List listMembers() throws DataAccessException;
+}
