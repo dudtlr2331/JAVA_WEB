42cad121afcefc46a33c543407dbf57e176cedfe
Cat Hanbit <dudtlr102@naver.com>
2022-08-29 22:49:54 +0900

    dao

---
+++ java_web/src/pro22/dao/MemberDAO.java
@@ -0,0 +1,14 @@
+package pro22.dao;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.spring.member.vo.MemberVO;
+
+
+public interface MemberDAO {
+	public List selectAllMembers() throws DataAccessException ;
+	public int addMember(MemberVO memberVO) throws DataAccessException ;
+
+}
